8e25917cd4b5c78829bf342016feaa449f616503
Milan Stojanovic <milan.stojanovic@upsideo.net>
2022-07-20 18:00:50 +0200

    Second part completed.

--- js/main-02.js
+++ js/main-02.js
@@ -204,3 +204,286 @@
 // } else {
 //   function foo() { console.log("b") }
 // }
+
+
+// CLOSURE
+// before closure
+// function foo() {
+//   var a = 2;
+
+//   function bar() {
+//     console.log(a); // 2
+//   }
+
+//   bar();
+// }
+
+// foo();
+
+// with closure
+// function foo() {
+//   var a = 2;
+
+//   function bar() {
+//     console.log(a);
+//   }
+
+//   return bar;
+// }
+
+// var baz = foo();
+// baz(); // 2
+
+// another example
+// function foo() {
+//   var a = 2;
+
+//   function baz() {
+//     console.log(a); // 2
+//   }
+
+//   bar(baz);
+// }
+
+// function bar(fn) {
+//   fn(); // closure
+// }
+
+// foo();
+
+// or
+// var fn;
+
+// function foo() {
+//   var a = 2;
+
+//   function baz() {
+//     console.log(a);
+//   }
+
+//   fn = baz; // add baz to global variable
+// }
+
+// function bar() {
+//   fn(); // closure
+// }
+
+// foo();
+
+// bar(); // 2
+
+// sample
+// function wait(msg) {
+//   setTimeout(function timer() {
+//     console.log(msg);
+//   }, 1000);
+// }
+
+// wait('closure');
+
+// sample of closure in jQuery
+// function setupBot(name, selector) {
+//   $(selector).click(function activator() {
+//     console.log("Acitvating " + name);
+//   });
+// }
+
+// setupBot("Closure bot 1", "#bot_1");
+// setupBot("Closure bot 2", "#bot_2");
+
+// when we pass callback function as parameter, closure will be created somwehre
+
+// closure in loops
+// not valid example
+// for (var i = 1; i <= 5; i++) {
+//   setTimeout(function timer() {
+//     console.log(i);
+//   }, i * 1000);
+// }
+
+// valid example
+// for (var i = 1; i <= 5; i++) {
+//   (function() {
+//     var j = i;
+//     setTimeout(function timer() {
+//       console.log(j);
+//     }, j * 1000);
+//   })();
+// }
+
+// or
+// for (var i = 1; i <= 5; i++) {
+//   (function(j) {
+//     setTimeout(function timer() {
+//       console.log(j);
+//     }, j * 1000);
+//   })(i);
+// }
+
+// or
+// for (var i = 1; i <= 5; i++) {
+//   let j = i;
+//   setTimeout(function timer() {
+//     console.log(j);
+//   }, j * 1000);
+// }
+
+// or
+// for (let i = 1; i <= 5; i++) {
+//   setTimeout(function timer() {
+//     console.log(i);
+//   }, i * 1000);
+// }
+
+// MODULS
+// before moduls
+// function foo() {
+//   var something = "cool";
+//   var another = [1, 2, 3];
+
+//   function doSomething() {
+//     console.log(something);
+//   }
+
+//   function doAnother() {
+//     console.log(another.join(' ! '));
+//   }
+// }
+
+// module concept - revealing module
+// function CoolModule() {
+//   var something = "cool";
+//   var another = [1, 2, 3];
+
+//   function doSomething() {
+//     console.log(something);
+//   }
+
+//   function doAnother() {
+//     console.log(another.join(' ! '));
+//   }
+
+//   return {
+//     doSomething: doSomething,
+//     doAnother: doAnother
+//   }
+// }
+
+// var foo = CoolModule();
+
+// foo.doSomething(); // cool
+// foo.doAnother(); // 1 ! 2 ! 3
+
+// unique module
+// var foo = (function CoolModule() {
+//   var something = "cool";
+//   var another = [1, 2, 3];
+
+//   function doSomething() {
+//     console.log(something);
+//   }
+
+//   function doAnother() {
+//     console.log(another.join(' ! '));
+//   }
+
+//   return {
+//     doSomething: doSomething,
+//     doAnother: doAnother
+//   }
+// })();
+
+// foo.doSomething(); // cool
+// foo.doAnother(); // 1 ! 2 ! 3
+
+// modules can have parameters
+// function CoolModule(id) {
+//   function identity() {
+//     console.log(id);
+//   }
+
+//   return {
+//     identity
+//   }
+// }
+
+// var foo1 = CoolModule("foo1");
+// var foo2 = CoolModule("foo2");
+
+// foo1.identity();
+// foo2.identity();
+
+// modules with named object
+// var foo = (function CoolModule(id) {
+//   function change() {
+//     publicAPI.identify = identify2;
+//   }
+
+//   function identify1() {
+//     console.log(id);
+//   }
+
+//   function identify2() {
+//     console.log(id.toUpperCase());
+//   }
+
+//   var publicAPI = {
+//     change,
+//     identify: identify1
+//   }
+
+//   return publicAPI;
+// })("foo module");
+
+// foo.identify(); // foo module
+// foo.change();
+// foo.identify(); // FOO MODULE
+
+// another example
+// var MyModules = (function Manager() {
+//   var modules = {};
+
+//   function define(name, deps, impl) {
+//     for (var i = 0; i < deps.length; i++) {
+//       deps[i] = modules[deps[i]];
+//     }
+//     modules[name] = impl.apply(impl, deps);
+//   }
+
+//   function get(name) {
+//     return modules[name];
+//   }
+
+//   return {
+//     define,
+//     get
+//   }
+// })();
+
+// MyModules.define("bar", [], function() {
+//   function hello(who) {
+//     return "Let me introduce: " + who;
+//   }
+
+//   return {
+//     hello
+//   }
+// });
+
+// MyModules.define("foo", ["bar"], function(bar) {
+//   var hungry = "hippo";
+
+//   function awesome() {
+//     console.log(bar.hello(hungry).toUpperCase());
+//   }
+
+//   return {
+//     awesome
+//   }
+// });
+
+// var bar = MyModules.get("bar");
+// var foo = MyModules.get("foo");
+
+// console.log(bar.hello("hippo"));
+// foo.awesome();
