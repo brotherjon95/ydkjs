2321fede289e871f579794454f5b0f3cf5fab3e6
Milan Stojanovic <milan.stojanovic@upsideo.net>
2022-08-23 15:37:17 +0200

    This completed.

--- js/main-03.js
+++ js/main-03.js
@@ -209,9 +209,151 @@
 // 4. Povezivanje funkcije pomocu operatora new
 
 // when we call function with word new in front of name of the function, totally new object is created and it is bounded with its own property [[Prototype]], this is inside that function and if it don't return alternative object, function which is related with operator new automatically returns new created object
-function foo(a) {
-  this.a = a;
-}
+// function foo(a) {
+//   this.a = a;
+// }
 
-var bar = new foo(2);
-console.log(bar.a); // 2
+// var bar = new foo(2);
+// console.log(bar.a); // 2
+
+// using this rules
+// function foo() {
+//   console.log(this.a);
+// }
+
+// var obj1 = {
+//   a: 2,
+//   foo: foo
+// };
+
+// var obj2 = {
+//   a: 3,
+//   foo: foo
+// };
+
+// obj1.foo(); // 2
+// obj2.foo(); // 3
+
+// obj1.foo.call(obj2); // 3
+// obj2.foo.call(obj1); // 2
+// foo(); // undefined
+
+// povezivanje pomocu new
+
+// function foo(something) {
+//   this.a = something;
+// }
+
+// var obj1 = {
+//   foo: foo
+// };
+
+// var obj2 = {};
+
+// obj1.foo(2);
+// console.log(obj1.a); // 2
+
+// obj1.foo.call(obj2, 3);
+// console.log(obj2.a); // 3
+
+// var bar = new obj1.foo(4);
+// console.log(obj1.a); // 2
+// console.log(bar.a); // 4
+
+
+
+// function foo(something) {
+//   this.a = something;
+// }
+
+// var obj1 = {};
+
+// var bar = foo.bind(obj1);
+// bar(2);
+// console.log(obj1.a); // 2
+// var baz = new bar(3);
+// console.log(obj1.a); // 2
+// console.log(baz.a); // 3
+
+// // ODREDJIVANJE STA THIS PREDSTAVLJA
+// 1. Da li je funkcija pozvana s operatorom new (povezivanje pomocu new)? Ako jeste, this predstavlja novokonstruisani objekat.
+// var bar = new foo()
+// 2. Da li je funkcija pozvana pomocu metode call ili apply (eksplicitno povezivanje), ili cak i umetnuta u cvrsto povezivanje pomocu funkcije bind?
+// Ako jeste, this predstavlja eksplicitno zadati objekat.
+// var bar = foo.call(obj2)
+// 3. Da li je funkcija pozvana sa zadatim kontekstom (implicitno povezivanje), sto je inace poznato kao vlasnistvo nad objektom ili sadrzavanje objekta?
+// Ako jeste, this predstavlja taj zadati objekat koneksta.Ako
+// var bar = obj1.foo()
+// 4. U ostalim slucajevima, this je podrazumevani objekat (podrazumevano povezivanje). Ako vazi striktni rezim, to je undefined objekat, a ukoliko ne vazi,
+// to je globalni objekat.Ako
+// var bar = foo()
+
+
+// DMZ objekat (demilitarizovana zona -  objekat koji cuva referencu na ceo kod u slucaju da this radi pogresno)
+// function foo (a, b) {
+//   console.log('a:' + a + 'b:' + b);
+// }
+
+// // nas prazni DMZ objekat
+// var dmzObj = Object.create(null);
+
+// // raspodela niza na parametre
+// foo.apply(dmzObj, [2, 3]); // a:2, b:3
+
+// // Parcanje pomocu bind(..)
+// var bar = foo.bind(dmzObj, 2);
+// bar(3); // a:2, b:3
+
+// LEKSICKI THIS - uz pomoc arrow funkcije koja povezuje this sa okruzujucim opsegom f-je
+// function foo() {
+//   // vraca f-ju sa strelicom
+//   return (a) => {
+//     // ovaj 'this' je leksicki nasledjen od foo()
+//     console.log(this.a);
+//   }
+// }
+
+// var obj1 = {
+//   a: 2
+// };
+
+// var obj2 = {
+//   a: 3
+// };
+
+// var bar = foo.call(obj1);
+// bar.call(obj2); // 2, ne 3!
+
+// OBJECTS
+// objekti mogu biti tipa string, number, boolean, null, undefined, object
+// Ugradjeni objekti - String, Number, Boolean, Object, Function, Array, Date, RegExp, Error
+
+// copying object
+// var newObj = Object.assign( {}, myObject );
+
+// PROPERTY DESCRIPTOR
+// var myObject = {
+//   a: 2
+// }
+
+// Object.getOwnPropertyDescriptor(myObject, 'a');
+// {
+//   value: 2,
+//   writable: true,
+//   enumerable: true,
+//   configurable: true
+// }
+
+// define property
+// var myObject = {};
+
+// Object.defineProperty(myObject, 'a', {
+//   value: 2,
+//   writable: true,
+//   enumerable: true,
+//   configurable: true
+// });
+
+// myObject.a; // 2
+
+// WRITABLE
